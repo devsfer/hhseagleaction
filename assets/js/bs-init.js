4d3a03f01d67d6c68c54829eecfceb4c004606a1
devsfer <74117879+devsfer@users.noreply.github.com>
2022-02-11 18:19:57 -0600

    V 1.6

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -1,3 +1,17 @@
+if (window.innerWidth < 768) {
+	$('[data-bss-disabled-mobile]').removeClass('animated').removeAttr('data-aos data-bss-hover-animate');
+}
+
 $(document).ready(function(){
+
+	$('[data-bss-hover-animate]')
+		.mouseenter( function(){ var elem = $(this); elem.addClass('animated ' + elem.attr('data-bss-hover-animate')) })
+		.mouseleave( function(){ var elem = $(this); elem.removeClass('animated ' + elem.attr('data-bss-hover-animate')) });
 	$('[data-bss-tooltip]').tooltip();
+
+	$('[data-bss-dynamic-product]').each(function(index, elem) {
+		var param = $(elem).data('bss-dynamic-product-param');
+		$(elem).attr('data-reflow-product', new URL(location.href).searchParams.get(param));
+	});
+
 });
